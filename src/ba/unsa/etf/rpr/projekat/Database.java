f1b12691cb7bb03bcea9d259e52e7164453daa9d
Lamija <lfazlija1@etf.unsa.ba>
2019-02-22 19:44:33 +0100

    updated models

--- src/ba/unsa/etf/rpr/projekat/Database.java
+++ src/ba/unsa/etf/rpr/projekat/Database.java
@@ -1,5 +1,56 @@
 package ba.unsa.etf.rpr.projekat;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
 public class Database {
+    private Connection conn;
+    private void regenerateDatabase() {
+        Scanner input = null;
+        try {
+            input = new Scanner(new FileInputStream("projectDatabase.db.sql"));
+            String sqlQuery = "";
+            while (input.hasNext()) {
+
+                sqlQuery += input.nextLine();
+                if (sqlQuery.charAt(sqlQuery.length() - 1) == ';') {
+                    try {
+
+                        Statement stmnt = conn.createStatement();
+                        stmnt.execute(sqlQuery);
+                        sqlQuery = "";
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        input.close();
+    }
+    public Database() {
+
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:projectDatabase.db");
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+        try {
+
+        } catch (Exception e) {
+            regenerateDatabase();
+
+        }
+
+    }
 
 }
