e5ff8ccf4e2666761a0f26e62ebba0856337f1ec
Lamija <lfazlija1@etf.unsa.ba>
2019-02-23 00:01:02 +0100

    added tree view of course list

--- src/ba/unsa/etf/rpr/projekat/CourseListController.java
+++ src/ba/unsa/etf/rpr/projekat/CourseListController.java
@@ -1,4 +1,52 @@
 package ba.unsa.etf.rpr.projekat;
 
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.StackPane;
+
 public class CourseListController {
-}
+    @FXML private TreeView<String> undergraduateTree;
+    @FXML private TreeView<String> masterTree;
+    @FXML private TreeView<String> phdTree;
+
+    public void initialize() {
+        loadUndergraduateItems("initial 1", "initial 2", "initial 3");
+        loadMasterItems("initial 1", "initial 2", "initial 3");
+        loadPhdItems("initial 1", "initial 2", "initial 3");
+
+    }
+
+    public void loadUndergraduateItems(String... rootItems) {
+        TreeItem<String> root = new TreeItem<String>("Undergraduate");
+        root.setExpanded(true);
+        for (String itemString: rootItems) {
+            root.getChildren().add(new TreeItem<String>(itemString));
+        }
+
+        undergraduateTree.setRoot(root);
+    }
+
+    public void loadMasterItems(String... rootItems) {
+        TreeItem<String> root = new TreeItem<String>("Master");
+        root.setExpanded(true);
+        for (String itemString: rootItems) {
+            root.getChildren().add(new TreeItem<String>(itemString));
+        }
+
+        masterTree.setRoot(root);
+    }
+
+    public void loadPhdItems(String... rootItems) {
+        TreeItem<String> root = new TreeItem<String>("Phd");
+        root.setExpanded(true);
+        for (String itemString: rootItems) {
+            root.getChildren().add(new TreeItem<String>(itemString));
+        }
+
+        phdTree.setRoot(root);
+    }
+
+    }
+
--- src/ba/unsa/etf/rpr/projekat/LoginFormController.java
+++ src/ba/unsa/etf/rpr/projekat/LoginFormController.java
@@ -3,12 +3,18 @@ package ba.unsa.etf.rpr.projekat;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public TextField usernameField;
@@ -26,14 +32,14 @@ public class LoginFormController {
         statusMsg.setVisible(false);
     }
 
-    public void Login(ActionEvent actionEvent) {
+    public void Login(ActionEvent actionEvent) throws IOException {
         String username = usernameField.getText();
         String password = passwordField.getText();
         ObservableList<Person> persons = database.getPersons();
         for (Person p : persons) {
             if (p.getFullName().equals(username)) {
                 if (p.getPassword().equals(password)) {
-                    //SUCCESS
+                    courseList();
                     statusMsg.setVisible(true);
                     statusMsg.setText("Please wait...");
                 } else {
@@ -50,6 +56,16 @@ public class LoginFormController {
                 }
             }
         }
+    }
+
+    private void courseList() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/courseList.fxml"));
+        CourseListController controller = new CourseListController();
+        loader.setController(controller);
+        Parent root = loader.load();
+        Stage primaryStage = (Stage) usernameField.getScene().getWindow();
+        primaryStage.setTitle("Course List");
+        primaryStage.setScene(new Scene(root, 600, 400));
     }
 
     public void LoginGuest(ActionEvent actionEvent) {
